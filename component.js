1c18e13e55272aa229b82ac7bc8fbe5405a447ba
ritorobbob <ritorobbob@gmail.com>
2017-03-20 18:59:17 +0000

    first actual commit

---
+++ component.js
@@ -0,0 +1,20 @@
+class component {
+    constructor(name) {
+        this.name = name;
+        this.x = 40;
+        this.y = 40;
+        this.size = [60, 60];
+    }
+    show() {
+        rectMode(CENTER)
+        fill(255);
+        rect(this.x, this.y, this.size[0], this.size[1]);
+    }
+    clicked() {
+        if ((mouseX > (this.x - (this.size[0]/2)) && mouseX < (this.x + (this.size[0]/2))) && 
+                (mouseY > (this.y -(this.size[1]/2)) && mouseY < (this.y + (this.size[1]/2)))) {
+        this.x = mouseX;
+        this.y = mouseY;
+        }
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,28 @@
+var img;
+var testComp;
+function preload() {
+  //img = loadImage("http://localhost:3000/dave.jpg");
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(120);
+  testComp = new component('comp');
+  console.log(component.name);
+}
+
+function draw() {
+  background(120);
+  testComp.show();
+  
+}
+
+function mouseDragged() {
+  testComp.clicked();
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
