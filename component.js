4e619740f9333dfc3bd9eff3c2c81464a3b9da9d
ritorobbob <ritorobbob@gmail.com>
2017-03-21 09:08:47 +0000

    Update component.js

--- component.js
+++ component.js
@@ -4,6 +4,7 @@ class component {
         this.x = 40;
         this.y = 40;
         this.size = [60, 60];
+        this.isMoving = false;
     }
     show() {
         rectMode(CENTER)
@@ -11,10 +12,12 @@ class component {
         rect(this.x, this.y, this.size[0], this.size[1]);
     }
     clicked() {
+        do{
         if ((mouseX > (this.x - (this.size[0]/2)) && mouseX < (this.x + (this.size[0]/2))) && 
                 (mouseY > (this.y -(this.size[1]/2)) && mouseY < (this.y + (this.size[1]/2)))) {
         this.x = mouseX;
         this.y = mouseY;
         }
+        while(this.isMoving)}
     }
 }
